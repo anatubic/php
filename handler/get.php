f8d85d62fea3792074a6a2de4e8ef0ad5842d120
anatubic <at20201025@student.fon.bg.ac.rs>
2023-08-18 19:50:08 +0200

    dodate ajax funkcionalnosti

---
+++ handler/get.php
@@ -0,0 +1,48 @@
+<?php
+    include '../databasebroker.php';
+    include '../model/komadodece.php';
+
+
+    if(isset($_POST['prikazid']) ){
+        $rez = KomadOdece::vratiKomadOdecePoID($_POST['prikazid'],$conn);
+
+        $response = array();
+        while($red=mysqli_fetch_assoc($rez)){
+            $response = $red;
+        }
+   
+        echo json_encode($response);
+
+    }else{
+        $response['status']=200; //status OK
+        $response['message']="Data not found";
+    }
+
+ 
+
+
+
+    
+
+    if(  isset($_POST['azurirajid'])){
+        $rez = KomadOdece::vratiKomadOdecePoID($_POST['azurirajid'],$conn);
+
+        $response = array();
+        while($red=mysqli_fetch_assoc($rez)){
+            $response = $red;
+        }
+        //echo $response;
+        echo json_encode($response);
+
+    }else{
+        $response['status']=200; //status OK
+        $response['message']="Data not found";
+    }
+
+
+
+
+
+
+
+?>
