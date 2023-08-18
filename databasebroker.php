ae2e26b6f81647935d5de6e6ba09ddf9fe3b757c
anatubic <at20201025@student.fon.bg.ac.rs>
2023-08-18 19:45:33 +0200

    konekcija na bazu

---
+++ databasebroker.php
@@ -0,0 +1,12 @@
+<?php
+ 
+     
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "butik";
+        
+        $conn = mysqli_connect($hostname,$username,$password,$database) or die("Database connection failed");
+     
+
+?>
